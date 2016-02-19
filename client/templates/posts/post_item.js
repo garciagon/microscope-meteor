306c3a8db44fdd849635aeaafced1943e8a8c4c4
garciagon <javier.garciagonzalez@bq.com>
2016-02-19 12:48:21 +0100

    Adding, deleting and restricting actions to users managing posts

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -2,6 +2,10 @@
 /* globals Template */
 
 Template.postItem.helpers({
+    ownPost: function() {
+        return this.userId === Meteor.userId();
+    },
+
     domain: function() {
         var a = document.createElement('a');
         a.href = this.url;
