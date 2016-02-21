995aa8755267d28e3f4553f0808f1fc43489d81a
garciagon <javier.garciagonzalez@bq.com>
2016-02-21 13:42:15 +0100

    Check if url exists when user updates a post

--- client/templates/posts/post_edit.js
+++ client/templates/posts/post_edit.js
@@ -2,24 +2,23 @@ Template.postEdit.events({
     'submit form': function(e) {
         e.preventDefault();
 
-        var currentPostId = this._id;
-
-        var postProperties = {
+        var postAttributes = {
             url: $(e.target).find('[name=url]').val(),
-            title: $(e.target).find('[name=title]').val()
+            title: $(e.target).find('[name=title]').val(),
+            _id: this._id
         }
 
-        Posts.update(currentPostId, {
-            $set: postProperties
-        }, function(error) {
-            if (error) {
-                // display the error to the user
-                alert(error.reason);
-            } else {
-                Router.go('postPage', {
-                    _id: currentPostId
-                });
-            }
+        Meteor.call('postUpdate', postAttributes, function(error, result) {
+            // display the error to the user and abort
+            if (error)
+                return alert(error.reason);
+            // show this result but route anyway
+            if (result.postExists)
+                alert('This link has already been posted.');
+
+            Router.go('postPage', {
+                _id: result._id
+            });
         });
     },
 
