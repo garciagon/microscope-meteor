3eda35ac800c6e42afe289f64b4348bbc35ad8b9
javiergarciagonzalez <javier.garciagonzalez@bq.com>
2016-02-26 17:16:57 +0100

    Get comments resources only on single post page

--- server/publications.js
+++ server/publications.js
@@ -5,6 +5,9 @@ Meteor.publish('posts', function() {
     return Posts.find();
 });
 
-Meteor.publish('comments', function() {
-    return Comments.find();
+Meteor.publish('comments', function(postId) {
+    check(postId, String);
+    return Comments.find({
+        postId: postId
+    });
 });
